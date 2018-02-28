d6e4e2d327f6c6aa9cb97daef4821b1abbc9bf93
florence <florence-mar@hotmail.fr>
2018-02-28 15:20:33 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+declare var google: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+@ViewChild('map') mapRef: ElementRef;
 
   constructor(public navCtrl: NavController) {
 
   }
+ionViewDidLoad(){
+ this.showMap();
+}
+showMap(){
+  const location = new google.maps.LatLng(510507351, -0.127758);
 
+  const options={
+    center: {lat: 510507351, lng: -0.127758},
+    zoom:2
+  }
+  const map= new google.maps.Map(this.mapRef.nativeElement,
+  options);
+
+    this.addMarker(location, map)
+}
+addMarker(position, map){
+return new google.maps.Marker({
+  position,
+  map
+})
+}
 }
