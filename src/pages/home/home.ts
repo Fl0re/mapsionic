eed14faae39ffd2cdb2228ea835e47e38cba85a1
florence <florence-mar@hotmail.fr>
2018-02-28 16:04:16 +0100

    second commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
 declare var google: any;
 @Component({
   selector: 'page-home',
@@ -8,9 +10,26 @@ declare var google: any;
 export class HomePage {
 @ViewChild('map') mapRef: ElementRef;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+ 
   }
+
+geo(){
+  this.geolocation.getCurrentPosition().then((resp) => {
+    // resp.coords.latitude
+    // resp.coords.longitude
+   }).catch((error) => {
+     console.log('Error getting location', error);
+   });
+   
+   let watch = this.geolocation.watchPosition();
+   watch.subscribe((data) => {
+    // data can be a set of coordinates, or an error (if an error occurred).
+    // data.coords.latitude
+    // data.coords.longitude
+   });
+}
+
 ionViewDidLoad(){
  this.showMap();
 }
@@ -18,14 +37,18 @@ showMap(){
   const location = new google.maps.LatLng(510507351, -0.127758);
 
   const options={
-    center: {lat: 510507351, lng: -0.127758},
+    center: location,
     zoom:2
   }
   const map= new google.maps.Map(this.mapRef.nativeElement,
   options);
-
+    this.geo()
     this.addMarker(location, map)
+   
 }
+
+
+
 addMarker(position, map){
 return new google.maps.Marker({
   position,
